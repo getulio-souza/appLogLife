e20dc3585055e2705af5ab32cef8b85dfdc20254
Getúlio Souza <getuliosouza@live.com>
2022-04-09 23:38:46 -0300

    comecei a ligar as telas/error500 expo

--- logLife/App.js
+++ logLife/App.js
@@ -1,158 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import React, {useState, useEffect} from 'react';
-import { View, KeyboardAvoidingView, Image, TextInput, TouchableOpacity, Text, StyleSheet, Animated } from 'react-native';
+import React from 'react';
+
+import { NavigationContainer, StackRouter } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Login from './Telas/Login';
+import Subscribe from '../Telas/subscribe'; 
+import Consulta from './Telas/Consulta'; 
+import escolher_opção from './Telas/Seleção'; 
+import Dados from './Telas/Dados_de_coleta'; 
+
+const Stack = createStackNavigator();
+
 
 export default function App() {
-
-  const [offset] = useState(new Animated.ValueXY({ x: 0, y: 95 }));
-  const [opacity] = useState(new Animated.Value(0));
-  
-  useEffect(() => {
-    Animated.parallel([
-      Animated.spring(offset.y, {
-        toValue: 0,
-        speed: 4,
-        bounciness: 20,
-      }),
-      Animated.timing(opacity, {
-        toValue: 1,
-        duration: 200,
-      })
-    ]).start();
-  }, []);
-
   return (
-    <KeyboardAvoidingView style={styles.background}>
-
-      <View style={styles.containerLogo}>
-        <Image source={require('./Img/logo_loglife.png')}
-        />
-      </View>
-       
-      <Animated.View style={[
-        styles.container,
-        {
-          opacity:opacity,
-          transform: [
-          {translateY: offset.y}
-        ]
-        }
-      ]}>
-
-        
-<TouchableOpacity style={styles.btn_title}>
-          <Text style={styles.btn_text}>Criar nova conta</Text>
-        </TouchableOpacity>
-      
-
-        <TextInput style={styles.input}
-          placeholder='Nome completo'
-          autoCorrect={false}
-          onChangeText={()=>{}}
-        />
-
-         <TextInput style={styles.input}
-          placeholder='Número de telefone'
-          keyboardType='number-pad'
-          autoCorrect={false}
-          onChangeText={()=>{}}
-        />
-
-         <TextInput style={styles.input}
-          placeholder='Endereço de e-mail'
-          autoCorrect={false}
-          onChangeText={()=>{}}
-        />
-
-        <TextInput style={styles.input}
-          placeholder='Senha'
-          autoCorrect={false}
-          onChangeText={()=>{}}
-        />
-
-        
-        <TouchableOpacity style={styles.btn_register}>
-          <Text style={styles.register_text}>Criar conta</Text>
-      </TouchableOpacity>
-
-      </Animated.View>
-
-    <StatusBar style="light" />
-    </KeyboardAvoidingView>
-  );
+    <NavigationContainer>
+      <Stack.Navigator>
+       <Stack.Screen name="Login" component={Login}/>
+       <Stack.Screen name="Subscribe" component={Subscribe}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+  
 }
 
-const styles = StyleSheet.create({
-  background: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#003275',
-  },
-
-  containerLogo:{
-    flex: 1,
-    justifyContent: 'center',
-  },
-
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: 90,
-    paddingBottom:40
-  },
-  
-  input: {
-    backgroundColor: '#fff',
-    width: 250,
-    height: 40,
-    marginBottom: 15,
-    color: '#222',
-    fontSize: 17,
-    borderRadius: 7,
-    paddingLeft:7,
-  },
-
-  btn_Submit: {
-    backgroundColor: '#61aadb',
-    width: 250,
-    height: 45,
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 7,
-    marginBottom: 15,
-  },
-
-  submit_text: {
-    color: '#fff',
-    fontSize: 18,
-  },
-
-  btn_register: {
-    backgroundColor: '#000',
-    width: 250,
-    height: 45,
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 7,
-    marginBottom: 15,
-
-  },
-
-  register_text: {
-    color:'#fff',
-  },
-      btn_title: {
-        width: 250,
-        height: 45,
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderRadius: 7,
-        marginBottom: 15,
-      },
-    btn_text: {
-        color: '#fff',
-        fontSize: 25,
-      }
-});
